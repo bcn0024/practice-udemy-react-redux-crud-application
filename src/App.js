ddc0cbb47c89f11882bcfe0f33cc05f6e47ac450
bcn0024 <bcn0024@icloud.com>
2020-05-11 22:47:55 +0900

    more jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+// Reactはjsxを使う時にトランスパイルするのに必要になる
 
 
 class App extends Component {
   render() {
-    return <h1>Hello World</h1>;
+    const greeting = <span>Hi!!</span>
+    const dom = <h1 className='foo'>{greeting}</h1>
+    // {}でjsxの中に変数も使用できる
+
+    // return返すjsxは１つでないといけない＝意図しないdivタグが生じる
+    // return (
+    //   <div>
+    //     {dom}
+    //     <label htmlFor='bar'>bar</label>
+    //     <input type='text' onChange={() => {console.log('OK')}} />
+    //   </div>
+    // )
+    return (
+      <React.Fragment>
+        {dom}
+        <label htmlFor='bar'>bar</label>
+        <input type='text' onChange={() => {console.log('OK')}} />
+      </React.Fragment>
+    )
+    // 🌟<React.Fragment>で意図しないdivタグを消すことができる
   }
   // jsxは下にトランスパイルされる
   // render(){
