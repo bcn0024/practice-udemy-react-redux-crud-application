2ee40d2e869b5b8d8ff52d8be0dd4ca412b6a09a
bcn0024 <bcn0024@icloud.com>
2020-05-13 21:27:43 +0900

    prop-types

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 // Reactはjsxを使う時にトランスパイルするのに必要になる
+import PropTypes from 'prop-types';
 
 // 🌸propsとはコンポーネントの属性のこと ex)props.nameやprop.ageなどあるデータ(文字列、配列、オブジェクトなど)の属性を参照できるもの、{}で渡す。
 
@@ -7,9 +8,9 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     const profiles = [
-      { name: '慎理', age: '25'},
-      { name: '美里', age: '24'},
-      { name: '名無し' }
+      { name: '慎理', age: 25},
+      { name: '美里', age: 24},
+      { name: '名無し', age: 100}
     ]
     return (
       <React.Fragment>
@@ -25,9 +26,11 @@ const User = (props) => {
 return <div>名前は{props.name}、年齢は{props.age}</div>
 }
 
-// propsを受け取るコンポーネントに表示するデフォルトなどに使用できる
-User.defaultProps = {
-  age: 1
+// 🌸propsの型の定義・・・大規模なコンポーネントだと型がわかりにくくなるため🌸
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
+  // 🌸isRequired で 必須項目になる
 }
 
 export default App;
